44c9d8759aa5b4d8c55da2bd1d6e11d0e2a5b2d9
nsherm01 <77041469+nsherm01@users.noreply.github.com>
2024-02-22 10:59:43 -0800

    inventory count

--- Assets/Scenes/Grid/PlacementSystem.cs
+++ Assets/Scenes/Grid/PlacementSystem.cs
@@ -16,6 +16,8 @@ public class PlacementSystem : MonoBehaviour
     [Tooltip("Toggle to turn off the grid")]
     [SerializeField] private GameObject _gridVisualization;
 
+
+
     private void Start()
     {
         StopPlacement();
