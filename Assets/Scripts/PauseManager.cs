d6882557b53e9ffb643c201627049436a27bfd07
nsherm01 <77041469+nsherm01@users.noreply.github.com>
2024-03-14 10:55:14 -0700

    Developed Haungs Mode

--- Assets/Scripts/PauseManager.cs
+++ Assets/Scripts/PauseManager.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class PauseManager : MonoBehaviour
 {
     public GameObject pauseMenu;
     private bool isPaused = false;
 
+    // Haungs Mode
+    public TextMeshProUGUI haungsText;
+    public Button haungsModeButton;
+    public GameObject genCoinButton;
+    private bool haungsModeEnabled = false;
 
+    void Start()
+    {
+        UpdateButtonText();
+        ToggleGenCoinVisibility();
+    }
 
     public void PauseGame()
     {
@@ -22,5 +33,31 @@ public class PauseManager : MonoBehaviour
             pauseMenu.SetActive(false);
 
         Time.timeScale = 1f;
+    }
+
+    public void ToggleHaungsMode()
+    {
+        haungsModeEnabled = !haungsModeEnabled;
+        UpdateButtonText();
+        ToggleGenCoinVisibility();
+    }
+
+    void UpdateButtonText()
+    {
+        haungsText.text = haungsModeEnabled ? "Disable Haungs Mode" : "Enable Haungs Mode";
+    }
+
+    void ToggleGenCoinVisibility()
+    {
+        if (genCoinButton != null)
+        {
+            genCoinButton.SetActive(haungsModeEnabled);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting game...");
+        Application.Quit();
     }
 }
